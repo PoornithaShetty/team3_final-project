87ce7444750148e9b9bcc9e29cab11899ab61f48
DenizBayram1 <93668202+DenizBayram1@users.noreply.github.com>
2021-12-18 10:09:13 +0300

    Update Footer.js

--- team milestones/frontendallpages/src/Footer.js
+++ team milestones/frontendallpages/src/Footer.js
@@ -16,7 +16,6 @@ function Footer(){
       <h5>Section</h5>
       <ul className="nav flex-column">
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
@@ -27,7 +26,6 @@ function Footer(){
       <h5>Section</h5>
       <ul className="nav flex-column">
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
@@ -38,7 +36,6 @@ function Footer(){
       <h5>Section</h5>
       <ul className="nav flex-column">
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
